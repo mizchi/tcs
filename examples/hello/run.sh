74c77fba41b84749ed260ea403d3ec7fd4791b4c
Koutarou Chikuba <miz404@gmail.com>
2015-05-17 16:03:36 +0900

    Add example

--- examples/hello/run.sh
+++ examples/hello/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
-../../tcs src/hello.tcs -o _tcs/hello.js -t flow
-flow && babel _tcs/hello.js -o lib/hello.js
+# ../../tcs src/hello.tcs -o _tcs/hello.js -t flow
+# flow && babel _tcs/hello.js -o lib/hello.js
 
-tcs -o _tcs src/**/*.tcs -t flow
-tcs -o lib src/**/*.tcs -t es5
+../../tcs -s src -o _tcs -t flow
+flow
+../../tcs -s src -o lib  -t es5
